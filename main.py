448106b1738d3edce7a0c8076be940f510f34c98
hunter <ryan.cu1200@gmail.com>
2023-05-19 12:08:46 +0530

    added Complete method to delete and auto refresh

--- main.py
+++ main.py
@@ -3,12 +3,27 @@ import functions
 
 checkBox = functions.ReadFunction()
 
+
+def Add_tod():
+    todo = st.session_state['input_todo'] + '\n'
+    checkBox.append(todo)
+    functions.WriteFunction(checkBox)
+
+
 st.title('Welcome to Todo ')
 st.subheader('Add things To Do')
 st.write('Whenever  Ideas come to mind ,Use TO dO')
 
-# for box in checkBox:
-#     st.checkbox(box, key=box)
+for index, box in enumerate(checkBox):
+    result = st.checkbox(box, key=box)
+    if result:
+        checkBox.pop(index)
+        functions.WriteFunction(checkBox)
+        del st.session_state[box] # delete console date as well
+        st.experimental_rerun() #this will refresh after deleting
 
-st.text_input(label='type', placeholder='Add new Todo...')
-print('hi there')
+
+st.text_input(label='type', on_change=Add_tod, placeholder='Add new Todo...',
+              key='input_todo', )
+
+st.session_state
